19a58a118a12fa3d117da1b6d3db33f6c0c59868
irvin <chavisseo@outlook.es>
2025-03-23 21:51:42 -0600

    Añade pantalla de Sign In y su boton de acceso en la bienvenida

--- app/src/main/java/com/example/proyectoz/SignIn.kt
+++ app/src/main/java/com/example/proyectoz/SignIn.kt
@@ -1,29 +1,20 @@
 package com.example.proyectoz
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
-
+class SignIn : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_sign_in)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        val btnLogin = findViewById<Button>(R.id.btn_login)
-        btnLogin.setOnClickListener(){
-            val intent = Intent(this, SignIn::class.java)
-            startActivity(intent)
         }
     }
 }
