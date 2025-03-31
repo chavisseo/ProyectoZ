86f72b203c623e1f6ba3ce7c0c6ea85288634fd7
irvin <chavisseo@outlook.es>
2025-03-30 20:58:11 -0600

    Añade pantalla de Sign Up (Registrar)

--- app/src/main/java/com/example/proyectoz/SignUp.kt
+++ app/src/main/java/com/example/proyectoz/SignUp.kt
@@ -1,34 +1,20 @@
 package com.example.proyectoz
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
-
+class SignUp : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_sign_up)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        val btnLogin = findViewById<Button>(R.id.btn_login)
-        val btnRegister = findViewById<Button>(R.id.btn_register)
-        btnLogin.setOnClickListener(){
-            val intent = Intent(this, SignIn::class.java)
-            startActivity(intent)
-        }
-        btnRegister.setOnClickListener(){
-            val intent = Intent(this, SignUp::class.java)
-            startActivity(intent)
         }
     }
 }
